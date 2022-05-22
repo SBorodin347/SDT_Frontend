b5e4810f37eaeb032011787f42920963e6f3aac9
Gelmoxx <djtoso@gmail.com>
2022-05-22 11:48:38 +0200

    Routing dokončený

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,10 +11,22 @@ import {AuthService} from "../services/authentication/auth.service";
 
 export class MenuComponent {
 
-  constructor(private permissionService: NgxPermissionsService, private auth: AuthService) {
+  constructor(private permissionService: NgxPermissionsService, private auth: AuthService, private router: Router) {
   }
   ngOnInit() {
     this.permissionService.loadPermissions(this.auth.getUserRoles());
+  }
+
+  public goCourses(){
+    this.router.navigate(['/courses']);
+  }
+
+  public goSecurity(){
+    this.router.navigate(['/security']);
+  }
+
+  public goManual(){
+    this.router.navigate(['/organization']);
   }
 
 
